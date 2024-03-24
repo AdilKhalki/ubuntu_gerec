64f1d4d25e3fb47a230624d6ea2d9db6541b5a54
AdilKhalki <adilkhalki@gmail.com>
2024-03-24 21:47:26 +0000

    mod2

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Grade extends Model
 {
     use HasFactory;
+    public function agents(){
+        return $this->hasMany(Agent::class);
+    }
 }
--- app/Models/Reclamation.php
+++ app/Models/Reclamation.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Reclamation extends Model
 {
     use HasFactory;
+
+
 }
